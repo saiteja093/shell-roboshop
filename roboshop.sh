d3ae5aa28f0e77cc7e8c0069fe965bb10e088228
saiteja <saiteja093@gmail.com>
2026-08-10 13:54:44 +0530

    shellrobo

--- roboshop.sh
+++ roboshop.sh
@@ -12,21 +12,17 @@ do
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
   --query "Instances[0].InstanceId" \
   --output text)
-  
+
  if [ "$instance" == "frontend" ]; then 
-   IP=$(
-      aws ec2 describe-instances \  
-      --instance-ids $INSTANCE_ID \  
-      --query "Reservations[].Instances[].PublicIpAddress" \  
-      --output text 
-    )
+   IP=$(aws ec2 describe-instances \
+ --instance-ids $INSTANCE_ID \
+ --query "Reservations[].Instances[].PublicIpAddress" \
+ --output text)
   else
-   IP=$(
-      aws ec2 describe-instances \  
-      --instance-ids $INSTANCE_ID \  
-      --query "Reservations[].Instances[].PrivateIpAddress" \  
-      --output text 
-    ) 
+   IP=$(aws ec2 describe-instances \
+ --instance-ids $INSTANCE_ID \
+ --query "Reservations[].Instances[].PrivateIpAddress" \
+ --output text) 
   fi 
   echo "IP address: $IP"
 done
