f5ab02cae32cdfe1c116c746d55e7b6545eeb5c4
saiteja <saiteja093@gmail.com>
2026-08-10 11:53:47 +0530

    shellrobo

--- roboshop.sh
+++ roboshop.sh
@@ -5,13 +5,30 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for insatnce in $@
 do 
-  INSTANCE_ID=$( aws ec2 run-instances \
+   INSTANCE_ID=$( aws ec2 run-instances \
    --image-id $AMI_ID \
    --instance-type t3.micro \
    --security-group-ids $SG_ID \
-   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=mango}]" \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
    --query 'instances[0].Instanceid[]' \  
    --output text )  
+
+  if [ $instance == "frontend" ]; then 
+   IP=$(
+      aws ec2 describe-instances \  
+      --instance-ids $INSTANCE_ID \  
+      --query "Reservations[].Instances[].PublicIpAddress" \  
+      --output text 
+    )
+  else
+   IP=$(
+      aws ec2 describe-instances \  
+      --instance-ids $INSTANCE_ID \  
+      --query "Reservations[].Instances[].PrivateIpAddress" \  
+      --output text 
+    ) 
+  fi 
+  echo "IP address: $IP"
 done
 
 
