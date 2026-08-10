b278a839dab7c36e833ba17fa3fbce23b5e4588d
saiteja <saiteja093@gmail.com>
2026-08-10 13:45:52 +0530

    shellrobo

--- roboshop.sh
+++ roboshop.sh
@@ -5,12 +5,14 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for insatnce in $@
 do 
-   INSTANCE_ID=$( aws ec2 run-instances \
-   --image-id $AMI_ID \
-   --instance-type t3.micro \
-   --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mongo}]' \
-   --query 'instances[0].Instanceid[]' \  
-   --output text )  
+   INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type t3.micro \
+  --security-group-ids $SG_ID \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+  --query "Instances[0].InstanceId" \
+  --output text)
+  
  if [ "$instance" == "frontend" ]; then 
    IP=$(
       aws ec2 describe-instances \  
