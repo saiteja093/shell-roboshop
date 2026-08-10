f71b9f300c92e505d614a9ebc52b295abe8df5e7
saiteja <saiteja093@gmail.com>
2026-08-10 11:20:12 +0530

    shell robo

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SG_ID="sg-0cbd61e0717109130"  
+AMI_ID="ami-0220d79f3f480ecf5" 
+
+for insatnce in $@
+do 
+  INSTANCE_ID=$( aws ec2 run-instances \
+   --image-id $AMI_ID \
+   --instance-type t3.micro \
+   --security-group-ids $SG_ID \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+   --query 'instances[0].Instanceid[]' \  
+   --output text )  
+done
+
+
+
+
+  
