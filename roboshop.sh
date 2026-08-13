57eb247dac105f47bc5ab21558c171b28a95a1b2
saiteja <saiteja093@gmail.com>
2026-08-13 13:58:30 +0530

    shellrobo

--- roboshop.sh
+++ roboshop.sh
@@ -10,16 +10,16 @@ for instance in $@
 do 
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type t3.micro \
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query "Instances[0].InstanceId" \
     --output text )
 
-  if [ "$instance" == "frontend" ]; then 
+  if [ $instance == "frontend" ]; then 
       IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
-    --query "Reservations[].Instances[].PublicIpAddress" \
+    --query 'Reservations[].Instances[].PublicIpAddress' \
     --output text)
     RECORD_NAME="$DOMAIN_NAME"  # daws88a.online
     
@@ -27,10 +27,13 @@ do
     
       IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
-    --query "Reservations[].Instances[].PrivateIpAddress" \
+    --query 'Reservations[].Instances[].PrivateIpAddress' \
     --output text) 
     RECORD_NAME="$INSTANCE.$DOMAIN_NAME" # mango.daws88a.online
   fi 
+      
+      echo "IP address is: $IP"
+      
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
@@ -53,6 +56,8 @@ do
     ]
   }
 '
+
+echo "record update for $INSTANCE"
 
 done
 
