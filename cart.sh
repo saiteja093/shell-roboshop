e67d46eecdba503e59010e4928770a7f38ac469f
saiteja <saiteja093@gmail.com>
2026-08-25 13:44:42 +0530

    cart

---
+++ cart.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/log/shell-roboshop"
+logs_file="$log_folder/$0.log"
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+script_dir=$PWD
+MONGODB_host=mongodb.daws88a.online
+
+if [ $userid -ne 0 ]; then
+   echo -e "$r please run this script with root user access $n" | tee -a $logs_file
+   exit 1
+fi  
+
+mkdir -p $logs_folder
+#### by default shell will not execute, only exectue when it will call
+
+validate()
+{
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 ..... $r failure $n" | tee -a $logs_file
+       exit 1
+   else
+       echo -e "$2 ......$y success $n" | tee -a $logs_file
+    fi        
+}
+
+dnf module disable nodejs -y &>>logs_file
+validate $? "disabling current version"
+
+dnf module enable nodejs:20 -y &>>logs_file
+validate $? "enabeling 20 version"
+
+dnf install nodejs -y &>>logs_file
+validate $? "installing nodejs"
+
+id roboshop &>>logs_file
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>logs_file
+    validate $? "creating system user"
+else
+
+    echo -e "user is alredy exist $r skippying $n"
+fi
+
+mkdir -p /app 
+validate $? "creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>logs_file
+validate $? "downloading code"
+
+cd /app 
+validate $? "moving to app"
+
+rm -rf /app/*
+validate $? "removing existed contant"
+
+unzip -n /tmp/cart.zip &>>logs_file
+validate $? "unziping catalouge in app directory"
+
+npm install &>>logs_file
+validate $? "installing dependences"
+
+cp $script_dir/cart.service /etc/systemd/system/cart.service
+validate $? "created systemctl serivces"
+
+systemctl daemon-reload
+systemctl enable cart &>>logs_file
+systemctl start cart &>>logs_file
+validate $? "starting and enabeling cart"
+
