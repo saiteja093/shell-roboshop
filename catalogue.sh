25f9777ceae06984f8b52554192e880f2190aaf7
saiteja <saiteja093@gmail.com>
2026-08-30 12:39:33 +0530

    my payment

--- catalogue.sh
+++ catalogue.sh
@@ -161,14 +161,15 @@ VALIDATE $? "Starting and enabling catalogue"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host "$MONGODB_HOST" --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
-if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js
+if [ "$INDEX" -eq -1 ]; then
+    mongosh --host "$MONGODB_HOST" </app/db/master-data.js
     VALIDATE $? "Loading products"
 else
     echo -e "Products already loaded ... $Y SKIPPING $N"
 fi
+
 
 systemctl restart catalogue
 VALIDATE $? "Restarting catalogue"
