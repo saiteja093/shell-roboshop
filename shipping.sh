09ff14f91685aabd809a47782f95c925a18b081a
saiteja <saiteja093@gmail.com>
2026-08-30 18:18:20 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -65,9 +65,9 @@ VALIDATE $? "Created systemctl service"
 dnf install mysql -y  &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
-if [ $? -ne 0 ]; then
 
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "SHOW DATABASES LIKE 'cities';"
+if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
